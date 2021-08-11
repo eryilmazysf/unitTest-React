40938b05a67d47208a1d314dc3a6cbe6dd593fc5
Yusuf Eryilmaz <yusuferyilmaz1819@gmail.com>
2021-08-11 17:40:12 -0400

    unit test practice for reactjs

---
+++ src/__test__/App.test.js
@@ -0,0 +1,75 @@
+import { render, screen } from "@testing-library/react";
+import App from "../App";
+import userEvent from "@testing-library/user-event";
+import axios from "axios";
+
+jest.mock("axios");
+
+beforeEach(() => {
+  render(<App />);
+}); //render app before each test function
+
+test("Logo must have src and alt attribute", () => {
+  //  render(<App/>);
+  screen.debug(); // screen is similar console log
+
+  const logo = screen.getByRole("img");
+  expect(logo).toHaveAttribute("src", "clarusway_logo.png");
+  expect(logo).toHaveAttribute("alt", "Clarusway");
+});
+
+test("header renders with correct text", () => {
+  // render(<App/>);
+  const headerElement = screen.getByText("Clarusway News App");
+  // expect(headerElement).toBeInTheDocument()
+  expect(headerElement.textContent).toBe("Clarusway News App");
+});
+
+test("search text and button text should be in app", () => {
+  // const searchElement = screen.getByText(/Search/);
+  // expect(searchElement).toBeInTheDocument();
+  expect(screen.getByText(/Search/)).toBeInTheDocument();
+
+  const buttonText = screen.getByText(/Go to news/);
+  expect(buttonText).toBeInTheDocument();
+});
+
+test("input contains initial value", () => {
+  const inputElement = screen.getByTestId("input");
+  expect(inputElement).toHaveValue("");
+});
+
+test("test input change", () => {
+  const inputElement = screen.getByTestId("input");
+  expect(inputElement).toHaveValue("");
+  userEvent.type(inputElement, "");
+});
+
+test("should display a text before loading data", () => {
+  const loading = screen.getByTestId("loading");
+  expect(loading).toBeInTheDocument("No news....");
+});
+
+test("fetches stories from an API and display them", async () => {
+  const stories = [
+    { objectID: "1", title: "Hello", url: "http" },
+    { objectID: "2", title: "React", url: "http" },
+  ];
+
+  const search = "";
+
+  const promise = Promise.resolve({ data: { hits: stories } });
+  axios.get.mockImplementationOnce(() => promise);
+  await expect(promise).resolves.toEqual({ data: { hits: stories } });
+
+  userEvent.click(
+    screen.getByRole("button", {
+      name: /go to news/i,
+    })
+  );
+  expect(axios.get).toHaveBeenCalledWith(
+    `http://hn.algolia.com/api/v1/search?query=${search}`
+  );
+  const items = await screen.findAllByRole("listitem");
+  expect(items).toHaveLength(2);
+});
